217578fc0bf366769c835b68313913f9e6e0c6fc
Alberto-18 <66730701+Alberto-18@users.noreply.github.com>
2020-12-21 12:38:24 +0100

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -9,15 +9,39 @@
 // Seccion 3----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function imprimirHoraFecha() {
     let contenedor = document.getElementById("seccion3");
-    var d = new Date();
-    contenedor.write(
-        'Fecha: '+d.getDate(),
-        '<br>Dia de la semana: '+d.getDay(),
-        '<br>Año: '+d.getFullYear(),
-        '<br>Hora: '+d.getHours(),
-        '<br>Hora UTC: '+d.getUTCHours(),
-        '<br>Minutos: '+d.getMinutes(),
-        
-    );
+    class Fecha {
+        constructor(dia, mes, ano) {
+            this.dia = dia;
+            this.mes = mes;
+            this.ano = ano;
+        }
+    
+        static hoy() {
+            var fecha = new Date();
+            var dia = fecha.getDate();
+            var mes = fecha.getMonth() + 1;
+            var ano = fecha.getFullYear();
+            return new Fecha(dia, mes, ano);
+        }
+    
+        toString(){
+            return this.ano;
+        }
+    }
+    
+    contenedor.innerHTML(Fecha.hoy());
+    
+    if(dia == 6 || dia == 0){
+        seccion.innerHTML = "En días festivos no es posible dar de alta nuevos libros";
+        boton.disabled = true;
+    }
+  
+    if((dia >= 1 && dia <= 5) && (hora >= 9 && hora <= 19)){
+        seccion.innerHTML = "Sección de alta abierta";
+    }
 
+    if((dia >= 1 && dia <= 5) && (hora >= 19 && hora <= 9)){
+        seccion.innerHTML = "Está fuera de horario. Solo es posible dar de alta libros de lunes a viernes de 9:00 a 19:00";
+        boton.disabled = true;
+    }
 }
