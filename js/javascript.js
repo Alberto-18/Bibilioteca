f282b03a9d513efb647f489d2ae3367eb5cb70ea
Alberto-18 <66730701+Alberto-18@users.noreply.github.com>
2020-12-21 19:52:25 +0100

    Update javascript.js
    Sección 3: imprimir mensaje de compra posible

--- js/javascript.js
+++ js/javascript.js
@@ -1,47 +1,33 @@
-// Formulario----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Seccion1-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-
-// Seccion 2----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+//Seccion2-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-// Seccion 3----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-function imprimirHoraFecha() {
-    let contenedor = document.getElementById("seccion3");
-    class Fecha {
-        constructor(dia, mes, ano) {
-            this.dia = dia;
-            this.mes = mes;
-            this.ano = ano;
-        }
-    
-        static hoy() {
-            var fecha = new Date();
-            var dia = fecha.getDate();
-            var mes = fecha.getMonth() + 1;
-            var ano = fecha.getFullYear();
-            return new Fecha(dia, mes, ano);
-        }
-    
-        toString(){
-            return this.ano;
-        }
+//Seccion3-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function imprimirMensajeFecha() {
+    let seccion = document.getElementById("seccion3");
+    let dia = new Date().getDay();
+    let hora = new Date().getHours();
+    let minuto = new Date().getMinutes();
+    let button = document.getElementById("buton1");
+
+    if((dia >= 1 && dia <= 5) && (hora >= 9 && hora <= 19) && (minuto >= 0 && minuto <= 59)){
+        seccion.innerHTML = "Sección de alta abierta";
+        
     }
-    
-    contenedor.innerHTML(Fecha.hoy());
-    
+
     if(dia == 6 || dia == 0){
         seccion.innerHTML = "En días festivos no es posible dar de alta nuevos libros";
-        boton.disabled = true;
-    }
-  
-    if((dia >= 1 && dia <= 5) && (hora >= 9 && hora <= 19)){
-        seccion.innerHTML = "Sección de alta abierta";
-    }
+        button.disabled = true;
 
-    if((dia >= 1 && dia <= 5) && (hora >= 19 && hora <= 9)){
-        seccion.innerHTML = "Está fuera de horario. Solo es posible dar de alta libros de lunes a viernes de 9:00 a 19:00";
-        boton.disabled = true;
     }
+    
+    if((dia >= 1 && dia <= 5) && (hora >= 19 && hora <= 9) && (minuto >= 0 && minuto <= 59)){
+        seccion.innerHTML = "Está fuera de horario. Solo es posible dar de alta libros de lunes a viernes de 9:00 a 19:00";
+        button.disabled = true; 
+
+    }
+    
+
 }
